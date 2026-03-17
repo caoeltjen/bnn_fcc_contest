d2ab83139d9eaab584dbec513a098170152bdf14
Christopher Oeltjen <ca.oeltjen@ufl.edu>
2026-03-17 14:58:49 -0400

    more testbench properties

--- rtl/neuron/neuron_tb_depth.sv
+++ rtl/neuron/neuron_tb_depth.sv
@@ -229,4 +229,12 @@ module neuron_tb_depth #(
     assert property (p_last_to_valid_out)
         else $error("valid_out was not asserted exactly 2 cycles after last");
 
+    property p_exact_two_cycle_latency;
+        @(posedge clk) disable iff (rst)
+        (valid_in && last) |-> ##1 !valid_out ##1 valid_out;
+    endproperty
+
+    assert property (p_exact_two_cycle_latency)
+        else $error("Output latency is not exactly 2 cycles");
+
 endmodule
