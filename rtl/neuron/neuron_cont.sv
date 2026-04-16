01212799358104f8ab3868547f8e11203e258bf3
ca.oeltjen <ca.oeltjen@ece-lnx-07.ad.ufl.edu>
2026-04-16 11:49:35 -0400

    yet another issue located but fixable with my fifo thankfully

--- rtl/neuron/neuron_cont.sv
+++ rtl/neuron/neuron_cont.sv
@@ -13,19 +13,21 @@ module neuron_cont # (
 
     assign acc_en = valid_in;
 
-    logic last_r, last_r2;
+    logic last_r, last_r2, last_r2_d;
 
-    assign valid_out = last_r2; 
+    assign valid_out = last_r2 & ~last_r2_d; 
     assign acc_clr = last_r;
 
     always_ff @(posedge clk or posedge rst) begin
         if(rst) begin
             last_r <= 1'b0;
             last_r2 <= 1'b0;
+            last_r2_d <= 1'b0;
         end
         else begin
             last_r <= last;
             last_r2 <= last_r;
+            last_r2_d <= last_r2;
         end
     end
 
